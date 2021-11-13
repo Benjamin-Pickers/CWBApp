f2ab384f4b6f6d7b2477d6953a8cd4010003930c
Benjamin-Pickers <ben10pickers@gmail.com>
2021-11-13 14:57:43 -0500

    Added templates and url paths

--- CWBDataApp/urls.py
+++ CWBDataApp/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'CWBDataApp'
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('BatchCostTracking/', views.BatchCostTracking, name='BatchCost'),
+    path('MaterialTesting/', views.MaterialTesting, name='MaterialTest'),
 ]
--- CWBDataApp/views.py
+++ CWBDataApp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Batchcosttracking
 
 def index(request):
+    return render(request, 'CWBDataApp/index.html')
+
+def BatchCostTracking(request):
     return HttpResponse('Hello world')
+
+def MaterialTesting(request):
+    return HttpResponse('')
