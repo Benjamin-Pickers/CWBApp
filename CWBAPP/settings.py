cefd397c7958ac9dad7a764c80c96bb37540fc06
Benjamin-Pickers <ben10pickers@gmail.com>
2022-01-06 13:08:52 -0500

    Added settings for whitenoise library

--- CWBAPP/settings.py
+++ CWBAPP/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
